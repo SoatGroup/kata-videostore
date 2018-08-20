6d6b845aa604cc5cc891f65f9db2d5681398fc31
Romain Deneau <rdeneau@yahoo.fr>
2018-08-20 09:38:35 +0200

    Fix horizontal scroll

--- 2_Uncle_Bob_Full/Soat.CleanCode.VideoStore.UncleBobFull.Tests/VideoStoreTests.cs
+++ 2_Uncle_Bob_Full/Soat.CleanCode.VideoStore.UncleBobFull.Tests/VideoStoreTests.cs
@@ -15,7 +15,12 @@ namespace Soat.CleanCode.VideoStore.UncleBobFull.Tests
         public void TestSingleNewReleaseStatement()
         {
             _customer.AddRental(new Rental(new Movie("The cell", Movie.NEW_RELEASE), 3));
-            Assert.Equal("Rental Record for Fred\n\tThe cell\t9.0\nYou owed 9.0\nYou earned 2 frequent renter points \n", _customer.Statement());
+            Assert.Equal(
+                "Rental Record for Fred\n" +
+                "\tThe cell\t9.0\n" +
+                "You owed 9.0\n" +
+                "You earned 2 frequent renter points \n",
+                _customer.Statement());
         }
 
         [Fact]
@@ -24,7 +29,13 @@ namespace Soat.CleanCode.VideoStore.UncleBobFull.Tests
             _customer.AddRental(new Rental(new Movie("The cell",         Movie.NEW_RELEASE), 3));
             _customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.NEW_RELEASE), 3));
 
-            Assert.Equal("Rental Record for Fred\n\tThe cell\t9.0\n\tThe Tigger Movie\t9.0\nYou owed 18.0\nYou earned 4 frequent renter points \n", _customer.Statement());
+            Assert.Equal(
+                "Rental Record for Fred\n" +
+                "\tThe cell\t9.0\n" +
+                "\tThe Tigger Movie\t9.0\n" +
+                "You owed 18.0\n" +
+                "You earned 4 frequent renter points \n",
+                _customer.Statement());
         }
 
         [Fact]
@@ -32,7 +43,12 @@ namespace Soat.CleanCode.VideoStore.UncleBobFull.Tests
         {
             _customer.AddRental(new Rental(new Movie("The Tigger Movie", Movie.CHILDREN), 3));
 
-            Assert.Equal("Rental Record for Fred\n\tThe Tigger Movie\t1.5\nYou owed 1.5\nYou earned 1 frequent renter points \n", _customer.Statement());
+            Assert.Equal(
+                "Rental Record for Fred\n" +
+                "\tThe Tigger Movie\t1.5\n" +
+                "You owed 1.5\n" +
+                "You earned 1 frequent renter points \n",
+                _customer.Statement());
         }
 
         [Fact]
@@ -42,7 +58,14 @@ namespace Soat.CleanCode.VideoStore.UncleBobFull.Tests
             _customer.AddRental(new Rental(new Movie("8 1/2",                   Movie.REGULAR), 2));
             _customer.AddRental(new Rental(new Movie("Eraserhead",              Movie.REGULAR), 3));
 
-            Assert.Equal("Rental Record for Fred\n\tPlan 9 from Outer Space\t2.0\n\t8 1/2\t2.0\n\tEraserhead\t3.5\nYou owed 7.5\nYou earned 3 frequent renter points \n", _customer.Statement());
+            Assert.Equal(
+                "Rental Record for Fred\n" +
+                "\tPlan 9 from Outer Space\t2.0\n" +
+                "\t8 1/2\t2.0\n" +
+                "\tEraserhead\t3.5\n" +
+                "You owed 7.5\n" +
+                "You earned 3 frequent renter points \n",
+                _customer.Statement());
         }
     }
 }
