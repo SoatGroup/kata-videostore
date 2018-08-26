e1a922b1264ad68e2068238eb2507d185329dbe3
Romain Deneau <rdeneau@yahoo.fr>
2018-08-26 13:44:28 +0200

    Extract method `RentalLine()`

--- 2_Uncle_Bob_Full/Soat.CleanCode.VideoStore.UncleBobFull/Statement.cs
+++ 2_Uncle_Bob_Full/Soat.CleanCode.VideoStore.UncleBobFull/Statement.cs
@@ -46,46 +46,51 @@ namespace Soat.CleanCode.VideoStore.UncleBobFull
         {
             var rentalLines = "";
             foreach (var rental in _rentals)
-            {
-                var thisAmount = 0m;
-
-                //dtermines the amount for each line
-                switch (rental.Movie.PriceCode)
-                {
-                    case Movie.REGULAR:
-                        thisAmount += 2;
-                        if (rental.DaysRented > 2)
-                        {
-                            thisAmount += (rental.DaysRented - 2) * 1.5m;
-                        }
-
-                        break;
-                    case Movie.NEW_RELEASE:
-                        thisAmount += rental.DaysRented * 3;
-                        break;
-                    case Movie.CHILDREN:
-                        thisAmount += 1.5m;
-                        if (rental.DaysRented > 3)
-                        {
-                            thisAmount += (rental.DaysRented - 3) * 1.5m;
-                        }
-
-                        break;
-                }
-
-                FrequentRenterPoints++;
-
-                if (rental.Movie.PriceCode == Movie.NEW_RELEASE
-                    && rental.DaysRented > 1)
-                {
-                    FrequentRenterPoints++;
-                }
-
-                rentalLines += "\t" + rental.Movie.Title + "\t" + thisAmount.ToString("0.0", CultureInfo.InvariantCulture) + Environment.NewLine;
-                TotalAmount   += thisAmount;
-            }
+                rentalLines += RentalLine(rental);
 
             return rentalLines;
+        }
+
+        private string RentalLine(Rental rental)
+        {
+            var rentalLine = "";
+            var thisAmount = 0m;
+
+            //dtermines the amount for each line
+            switch (rental.Movie.PriceCode)
+            {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (rental.DaysRented > 2)
+                    {
+                        thisAmount += (rental.DaysRented - 2) * 1.5m;
+                    }
+
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += rental.DaysRented * 3;
+                    break;
+                case Movie.CHILDREN:
+                    thisAmount += 1.5m;
+                    if (rental.DaysRented > 3)
+                    {
+                        thisAmount += (rental.DaysRented - 3) * 1.5m;
+                    }
+
+                    break;
+            }
+
+            FrequentRenterPoints++;
+
+            if (rental.Movie.PriceCode == Movie.NEW_RELEASE
+                && rental.DaysRented > 1)
+            {
+                FrequentRenterPoints++;
+            }
+
+            rentalLine += "\t" + rental.Movie.Title + "\t" + thisAmount.ToString("0.0", CultureInfo.InvariantCulture) + Environment.NewLine;
+            TotalAmount += thisAmount;
+            return rentalLine;
         }
 
         private string Footer()
